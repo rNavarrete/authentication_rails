4d7f297abf2bff7d297c07b38f95626254628a36
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-27 01:12:42 -0600

    hashing works and configured views/form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the site!"
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem creating your account. Please try again."
+      redirect_to :back
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :display_name, :full_name, :admin_status)
+  end
+
+end
